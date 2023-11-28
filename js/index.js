45494c910da1897c9888186674c5045990d61e00
Hardeep <deshardeep@gmail.com>
2023-11-28 11:02:45 +0530

    Basics Of Javascript

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// var. let , const
+// 1.
+// let age = 30; // if i use const then i get error;
+//age = 31;
+// console.log(age);
+// 2.
+// const score = 10;
+// console.log(score);
+
+// String, Numbers, Bolean, null, Undefined
+// const name =`john`;
+// const age = 30;
+// const rating = 4.5;
+// const isCool =true;
+// const x = null;
+// const y = undefined;
+// let z;
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isCool);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+// /=================================================/
+
+// const name = `john`;
+// const age = 30;
+// concatenation
+// console.log(`My name is ` + name + ` and I am ` + age);
+// =====================================================
+// Template String
+// const hello = `My is ${name} And I am ${age}`;
+// console.log(hello);
+
+// ====================================================
+
