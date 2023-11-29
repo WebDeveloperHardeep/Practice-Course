7111791c00452b5b18adcc80bfd5e146cb00cfdb
Hardeep <deshardeep@gmail.com>
2023-11-29 13:39:55 +0530

    Using array of oject in js

---
+++ js/arrayofoject.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Tale out trash',
+        inCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        inCompleted: true
+    },    {
+        id: 3,
+        text: 'Dentist Appt',
+        inCompleted: false
+    }
+];
+
+// console.log(todos[1].text)
+
+// const todoJSON = JSON.stringify(todos);
+// console.log(todoJSON)
