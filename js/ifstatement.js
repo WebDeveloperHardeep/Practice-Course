211fe42ce9aee516e59551dc1cf2d0ff295726b9
Hardeep <deshardeep@gmail.com>
2023-11-29 14:47:57 +0530

    Using if, else if statement in js

---
+++ js/ifstatement.js
@@ -0,0 +1,18 @@
+// if statement
+
+const x = 4;
+const y = 11;
+
+if(x > 5 && y > 10){
+    console.log(' x is more then 5 or 5 is more than 10')
+}
+// else if(x >10 ){
+//     console.log('x is greter than 10')
+// }
+// else{
+//     console.log('x is Not 10');
+// }
+
+// if(x > 5){
+//     if(y > 10)  {}
+// };
