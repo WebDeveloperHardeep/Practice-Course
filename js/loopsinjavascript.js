59f70214ebffc9fea1f1438f9f8b93fa9d423164
Hardeep <deshardeep@gmail.com>
2023-11-29 13:53:15 +0530

    Using For loop & whille loop

---
+++ js/loopsinjavascript.js
@@ -0,0 +1,13 @@
+// for loops 
+// for(i = 0; i <= 10; i++){
+//     console.log(`For loop number: ${i}`);
+// }
+
+// While Loops
+
+// let i = 0;
+// while(i < 10){
+//     console.log(`While Loop Number: ${i}`)
+//     i++;
+// }
+
