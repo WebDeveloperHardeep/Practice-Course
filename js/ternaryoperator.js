bf6192395032a1d80b7134a08e6353f0d06a50df
Hardeep <deshardeep@gmail.com>
2023-11-29 15:19:33 +0530

    Using Ternaryoperator in js

---
+++ js/ternaryoperator.js
@@ -0,0 +1,15 @@
+const x = 9;
+
+// const color = x > 10 ? 'red' : 'blue';
+const color = 'green';
+// console.log(color)
+switch(color){
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+    console.log('color is blue');
+    break;
+    default:
+        console.log('color is not red or blue');
+}
